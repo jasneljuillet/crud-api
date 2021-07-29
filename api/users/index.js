ee235cf9455384f62cb7c5238bcf4898f6b401bb
JUILLET Jasnel <40551326+jasneljuillet@users.noreply.github.com>
2021-07-29 15:26:44 -0500

    endpoint for user

---
+++ api/users/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const { PrismaClient } = require('../../src/generated/client');
+const prisma = new PrismaClient();
+const  { findAll, create } = require('../../util/user');
+
+module.exports.findAll = async (req, res) => {
+    const users = await findAll();
+    res.json(users);
+    
+}
+
+module.exports.create = async (req, res) => {
+    const body = {
+        fullname: 'Vilsaint Ebrans',
+        email: 'ebrans@gmail.com'
+    };
+
+    const user = await create(body);
+    res.json(user);
+}
