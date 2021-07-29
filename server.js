36e49de723007fbf800c2c3a402d81f0a04d4427
JUILLET Jasnel <40551326+jasneljuillet@users.noreply.github.com>
2021-07-29 15:31:34 -0500

    added users endpoint

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
 
+
+app.get('/', (req, res) => res.json({ succes: true }));
+
+// user endpoint
+app.get('/api/users', require('./api/users').findAll);
+app.post('/api/users', require('./api/users').create);
+
+
 const PORT = 4500 || process.env.PORT;
 app.listen(PORT, () => console.log(`Listen on port ${PORT}`));
