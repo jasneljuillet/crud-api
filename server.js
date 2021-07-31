b03b271b6131e6a0a3c9c2f1c462b86e30fb5e1a
JUILLET Jasnel <40551326+jasneljuillet@users.noreply.github.com>
2021-07-31 10:18:48 -0500

    psot endpoints

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
 const user = require('./api/users/');
+const post = require('./api/post/');
 
 app.get('/', (req, res) => res.json({ succes: true }));
 
 // user endpoint
-app.get('/api/users', user.findAll);
 app.post('/api/users', user.create);
-app.delete('/api/users/', user.delete);
-app.put('/api/users/', user.update);
+app.get('/api/users', user.findAll);
+app.put('/api/users', user.update);
+app.delete('/api/users', user.delete);
+
+
+// post endpoint
+app.post('/api/post', post.create);
+app.get('/api/post', post.findAll);
+app.put('/api/post', post.update);
+app.delete('/api/post', post.delete);
 
 const PORT = 4500 || process.env.PORT;
 app.listen(PORT, () => console.log(`Listen on port ${PORT}`));
